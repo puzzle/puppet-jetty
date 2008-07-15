9ec1bbceb1b7c332ad3e7c339d93225f48998893
Marcel Haerry <haerry@puzzle.ch>
2008-07-15 11:40:28 +0200

    factored ssl out in a seperate file

---
+++ manifests/ssl.pp
@@ -0,0 +1,21 @@
+# manifests/ssl.pp
+# enabled ssl for jetty
+class jetty::ssl {
+    file{'/etc/jetty6/jetty-ssl.xml':
+        source => [ "puppet://$server/files/jetty/config/${fqdn}/jetty-ssl.xml",
+                    "puppet://$server/files/jetty/config/jetty-ssl.xml",
+                    "puppet://$server/jetty/config/jetty-ssl.xml" ],
+        require => Package['jetty6'],
+        notify => Service['jetty6'],
+        owner => root, group => 0, mode => 0644;
+    }
+
+    line { enable_jetty_ssl:
+        file => '/etc/jetty6/jetty.conf',
+        line => '/etc/jetty6/jetty-ssl.xml',
+        ensure => present,
+        notify => Service[munin-node],
+        require => Package[munin-node],
+  }
+}
+
